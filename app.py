b231d694f755b7bec6f0498020b1c8e14f778021
Riya Anadkat <riya.anadkat@gmail.com>
2024-03-22 21:06:22 -0400

    Create endpoint to capture frame

--- app.py
+++ app.py
@@ -1,6 +1,52 @@
-from flask import Flask, render_template
+# import cv2
+import base64
+from flask import Flask, render_template, request, jsonify
+from azure.cognitiveservices.vision.face import FaceClient
+from msrest.authentication import CognitiveServicesCredentials
+from dotenv import load_dotenv
+import numpy as np
+import os
+
+load_dotenv()
+
 
 app = Flask(__name__)
+
+
+app.config['KEY'] = os.environ.get('VISION_KEY')
+app.config['ENDPOINT'] = os.environ.get('VISION_ENDPOINT')
+
+face_client = FaceClient(app.config['ENDPOINT'], CognitiveServicesCredentials(app.config['KEY']))
+
+# def detect_age(image_path):
+#     h
+
+def detect_age(frame_data):
+    frame_np = np.frombuffer(frame_data, dtype=np.uint8)
+
+    # frame_np = np.array(frame_data, dytpe=np.uint8)
+    # _, img_encoded = cv2.imencode('.jpg', frame_np)
+    # image_stream = img_encoded.tobytes()
+    
+    # print(image_stream)
+    # detected_faces = face_client.face.detect_with_stream(image=image_stream, recognition_model='recognition_04', return_face_attributes=['age'])
+
+    # if not detected_faces:
+    #     print("No faces detected")
+    #     return False
+
+    # age = detected_faces[0].face_attribute.age
+    # print(f"Estimated age is {age}")
+    return True
+
+@app.route('/capture_frame', methods=['POST'])
+def capture_frame():
+    frame_data_base64 = request.json['frame']
+    frame_data = base64.b64decode(frame_data_base64.split(",")[1]) 
+
+    # print(frame_data)
+    detect_age(frame_data)
+    return jsonify({'message': 'Frame received successfully'})
 
 @app.route('/')
 def index():
---
+++ static/script.js
@@ -0,0 +1,41 @@
+function updateRandomNumber() {
+    var randomNumber = Math.floor(Math.random() * 101);
+    document.getElementById("random-number").textContent = randomNumber;
+}
+
+function getFrame() {
+    var video = document.getElementById('video');
+    var canvas = document.createElement('canvas');
+    var context = canvas.getContext('2d');
+
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight
+    context.drawImage(video, 0, 0, canvas.width, canvas.height)
+
+    var frameData = canvas.toDataURL('image/jpeg')
+
+    fetch('/capture_frame', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                frame: frameData
+            })
+        })
+        .catch(error => console.log(error))
+}
+
+navigator.mediaDevices.getUserMedia({
+        video: true
+    })
+    .then(function (stream) {
+        var video = document.getElementById('video');
+        video.srcObject = stream;
+    })
+    .catch(function (err) {
+        console.error('Error accessing the camera:', err);
+    });
+
+setInterval(getFrame, 3000);
+setInterval(updateRandomNumber, 200);
